28065a2abd3ae1dcb1bb7c401bc0643a8d9b98b2
Itay9 <itaydali@gmail.com>
2021-03-24 13:29:38 +0200

    register func parent + kid

--- src/DB init.py
+++ src/DB init.py
@@ -3,16 +3,14 @@ import sqlite3
 conn = sqlite3.connect("testDB.db")
 cursor = conn.cursor()
 #DB init table
-
-cursor.execute("""CREATE TABLE users
-            (userName text,
-            pass text,
-            type text)""")  # type is admin , parent or kid
-cursor.execute("INSERT INTO users VALUES ('itay','123','admin')")
-
-#db insert rows
-
-cursor.execute("INSERT INTO users VALUES ('yaron','123','parent')")
-cursor.execute("INSERT INTO users VALUES ('chen','123','kid')")
-cursor.execute("INSERT INTO users VALUES ('yaniv','123','kid')")
-conn.commit()
+def db_init():
+    cursor.execute("""CREATE TABLE users
+                (userName text,
+                pass text,
+                type text)""")  # type is admin , parent or kid
+    #db insert rows
+    cursor.execute("INSERT INTO users VALUES ('itay','123','admin')")
+    cursor.execute("INSERT INTO users VALUES ('yaron','123','parent')")
+    cursor.execute("INSERT INTO users VALUES ('chen','123','kid')")
+    cursor.execute("INSERT INTO users VALUES ('yaniv','123','kid')")
+    conn.commit()

6152687f082141b3e3527d7297d8766d91db57d3
Itay9 <itaydali@gmail.com>
2021-03-24 11:40:52 +0200

    login func

--- src/LoginManu.py
+++ src/LoginManu.py
@@ -6,16 +6,17 @@ cursor = conn.cursor()
 cursor.execute("SELECT * FROM users WHERE type = 'admin'")
 print("the admin is:",cursor.fetchone()[0])
 """
-user = input("enter user name: ")
-cursor.execute("SELECT * FROM users WHERE userName = '"+user+"'")
-fet = cursor.fetchone()
-if fet is None:
-    print("user does not exist!")
-else:
-    password = input("enter password: ")
-    if fet[1] == password:
-        print("login succss!")
+def login():
+    user = input("enter user name: ")
+    cursor.execute("SELECT * FROM users WHERE userName = '"+user+"'")
+    fet = cursor.fetchone()
+    if fet is None:
+        print("user does not exist!")
     else:
-        print("wrong password!")
+        password = input("enter password: ")
+        if fet[1] == password:
+            print("login succss!")
+        else:
+            print("wrong password!")
 
 

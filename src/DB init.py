c65f2df2645d0d032458f892c11797dda1d3d44b
Itay9 <itaydali@gmail.com>
2021-03-24 14:24:01 +0200

    register func Docstring

--- src/DB init.py
+++ src/DB init.py
@@ -1,16 +1,19 @@
 import sqlite3
 
-conn = sqlite3.connect("testDB.db")
+conn = sqlite3.connect("usersDB.db")
 cursor = conn.cursor()
 #DB init table
 def db_init():
     cursor.execute("""CREATE TABLE users
                 (userName text,
                 pass text,
-                type text)""")  # type is admin , parent or kid
+                type text,
+                parent)""")  # type is admin , parent or kid
     #db insert rows
-    cursor.execute("INSERT INTO users VALUES ('itay','123','admin')")
-    cursor.execute("INSERT INTO users VALUES ('yaron','123','parent')")
-    cursor.execute("INSERT INTO users VALUES ('chen','123','kid')")
-    cursor.execute("INSERT INTO users VALUES ('yaniv','123','kid')")
+    cursor.execute("INSERT INTO users VALUES ('itay','123','admin','')")
+    cursor.execute("INSERT INTO users VALUES ('yaron','123','parent','')")
+    cursor.execute("INSERT INTO users VALUES ('chen','123','kid','yaron')")
+    cursor.execute("INSERT INTO users VALUES ('yaniv','123','kid','yaron')")
     conn.commit()
+
+db_init()
--- src/LoginManu.py
+++ src/LoginManu.py
@@ -1,9 +1,18 @@
 import sqlite3
 
-conn = sqlite3.connect("testDB.db")
+conn = sqlite3.connect("usersDB.db")
 cursor = conn.cursor()
 
 def login(user,password):
+    """
+
+    Args:
+        user:
+        password:
+
+    Returns:
+
+    """
     cursor.execute("SELECT * FROM users WHERE userName = '"+user+"'")
     fet = cursor.fetchone()
     if fet is None:
@@ -18,20 +27,48 @@ def login(user,password):
             return False
 
 def register_parent(user,password):
+    """
+
+    Args:
+        user: string
+        password: string
+
+    Returns: register parent to DB
+
+    """
     cursor.execute("SELECT * FROM users WHERE userName = '" + user + "'")
     fet = cursor.fetchone()
     if fet is None:
         cursor.execute("INSERT INTO users VALUES ('"+user+"','"+password+"','parent')")
         print("register parent complete!")
     else:
-        print("user already exist!, select diffrent user name!")
+        print("user already exist!, select different user name!")
 
-def register_kid(user,password):
+def register_kid(user,password,parent):
+    """
+
+    Args:
+        user: name of the kid
+        password:
+        parent: name of parent
+
+    Returns: register new kid to DB
+
+    """
     cursor.execute("SELECT * FROM users WHERE userName = '" + user + "'")
     fet = cursor.fetchone()
     if fet is None:
         cursor.execute("INSERT INTO users VALUES ('"+user+"','"+password+"','kid')")
         print("register kid complete!")
     else:
-        print("user already exist!, select diffrent user name!")
+        print("user already exist!, select different user name!")
 
+def get_kids(parent):
+
+    pass
+#test
+"""
+login("itay","123") #ok
+login("yaron","11234") #wrong pass
+login("aa","aaa") #wrong user
+"""

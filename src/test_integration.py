ed4e234b8e4924a1df648dd92e2f1481e44e61b9
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2021-04-04 15:06:48 +0300

    Create test_integration.py
    Very first test

---
+++ src/test_integration.py
@@ -0,0 +1,12 @@
+import unittest
+import LoginManu
+
+class TestIntegration(unittest.TestCase):
+  """
+  """
+  
+  def test(self):
+    self.assertEqual(True,True)
+  
+if __name__ == '__main__':
+    unittest.main()

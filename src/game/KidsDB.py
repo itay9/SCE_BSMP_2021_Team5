d640bf71142085d0b0ee29362e20876037f955a3
Itay9 <itaydali@gmail.com>
2021-04-12 16:28:25 +0300

    Question DB

--- src/game/KidsDB.py
+++ src/game/KidsDB.py
@@ -1,23 +1,36 @@
 import sqlite3
 from datetime import datetime
 
-conn = sqlite3.connect("KidsDB.db")
-cursor = conn.cursor()
+connKids = sqlite3.connect("KidsDB.db")
+cursorKids = connKids.cursor()
+connQ = sqlite3.connect("questionDB.db")
+cursorQ = connQ.cursor()
 
 #DB init table
-def db_init():
-    cursor.execute("""CREATE TABLE users
+def init_kidDB():
+    cursorKids.execute("""CREATE TABLE users
                 (KidName text,
                 Date timestamp,
                 GameNumber INTEGER,
-                GameLog Blob
+                GameLog Blob,
                 GameSuccess Real)""")
     #gamalog = [(Qnumber,answer,correct)]*
     #GameSuccess = (number of correct / total Q) * 100 %
     #db insert rows
     #cursor.execute("INSERT INTO users VALUES ('chen','123','admin','')")
 
-    conn.commit()
+def init_QDB():
+    cursorQ.execute("""CREATE TABLE users
+                (QuestionID INTEGER,
+                Quesion text,
+                picture blob,
+                choice1 text,
+                choice2 text,
+                choice3 text,
+                choice4 text,
+                answer INTEGER)""")
+
+    connKids.commit()
 
 def stampToTime(timestamp):
     return datetime.fromtimestamp(timestamp)

1871b94473aa46b0ee0862a1d1b288c938b9833d
chenaz1 <57365377+chenaz1@users.noreply.github.com>
2021-04-19 05:11:26 +0300

    test_get_ques

--- src/game/gameDB.py
+++ src/game/gameDB.py
@@ -51,12 +51,14 @@ def add_question_to_qdb(quesion, picUrl, ch1, ch2, ch3, ch4, ans):
 
 
 def get_question_from_id(questionID):
-    cursor.execute("SELECT * FROM ques WHERE qid = ?", questionID)
+    # check if input is legal
+    cursor.execute("SELECT * FROM ques WHERE qid = ?", (questionID,))#send the INT as tuple is required
     fet = cursor.fetchone()
     if fet != None:
         return fet
     else:
         print("cant find question")
+        return
 
 
 def stampToTime(timestamp):
@@ -96,26 +98,6 @@ def get_qestion_id():
     cursor.execute("SELECT * FROM ques")
     fet = cursor.fetchall()
     return len(fet) + 1
-
-
-'''
-
-   @classmethod
-   def setUpClass(cls):
-       cls.cursor.execute("INSERT INTO ques VALUES (999,'firstQ','url','a','b','c','d',1)")
-       cls.cursor.execute("INSERT INTO ques VALUES (9999,'secondQ','url','e','f','g','h',2)")
-       cls.conn.commit()
-       print("setUp complete")
-
-   @classmethod
-   def tearDownClass(cls):
-       cls.cursor.execute("DELETE FROM ques WHERE qid= 999")
-       cls.cursor.execute("DELETE FROM ques WHERE qid= 9999")
-       cls.conn.commit()
-       print("tearDown complete")
-
-
-'''
 
 
 def build_db():
--- src/game/test_game_intergation.py
+++ src/game/test_game_intergation.py
@@ -3,6 +3,7 @@ import unittest
 import gameDB
 import os.path
 
+
 class GameTest(unittest.TestCase):
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     db_path = os.path.join(BASE_DIR, "gameDB.db")
@@ -26,6 +27,14 @@ class GameTest(unittest.TestCase):
 
     def test_check(self):
         self.assertEqual(True, True)
+
+    def test_get_ques(self):
+        self.cursor.execute("SELECT qid FROM ques WHERE qid = 999")
+        fet = self.cursor.fetchone()[0]  # first arg from fet(return tuple)
+        self.assertEqual(fet, 999)
+
+        res = gameDB.get_question_from_id(999)
+        print("question is:", res)
 
 
 if __name__ == '__main__':
